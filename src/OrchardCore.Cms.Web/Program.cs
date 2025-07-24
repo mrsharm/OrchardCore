4816c82970b5332e5352f4814716d513f1a11ef7
Mukund Raghav Sharma <musharm@microsoft.com>
2025-07-24 15:24:02 -0700

    Added info

--- src/OrchardCore.Cms.Web/Program.cs
+++ src/OrchardCore.Cms.Web/Program.cs
@@ -10,6 +10,10 @@ builder.Services
 
 var app = builder.Build();
 
+// DEMO: Intentional crash for demonstration purposes
+// This will cause the application to crash on startup
+throw new InvalidOperationException("Demo crash: This is an intentional exception to demonstrate error handling!");
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
